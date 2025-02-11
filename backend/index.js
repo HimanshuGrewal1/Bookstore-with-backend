3301d3bf4605c04881142819d4e4368693c7484a
HimanshuGrewal1 <g.himanshu2806@gmail.com>
2025-02-11 14:13:19 +0530

    WTF

--- backend/index.js
+++ backend/index.js
@@ -45,6 +45,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
    origin: ["http://localhost:5173",'https://mybookstore-murex.vercel.app'],
+   methods: "GET,POST,PUT,DELETE",
    credentials: true
 }));
 
